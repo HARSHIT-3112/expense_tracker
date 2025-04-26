95094e10b8ebb3b5bd1a40b5eb169f8f4d4b9227
HARSHIT-3112 <chaturvedih45@gmail.com>
2025-04-27 01:26:23 +0530

    updated on 27aprill

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4,primary_key=True, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+    
+
+
+class Transaction(BaseModel):
+    description = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ("description",)
+       
